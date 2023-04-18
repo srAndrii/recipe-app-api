6e9ee07d4a382f31137dc47d8de05d6bb970a669
srAndrii <55akif5548@gmail.com>
2023-04-17 17:51:10 -0700

    Write tests for updating recipe ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes',
-                  'price', 'link', 'tags', 'ingredients',]
+                  'price', 'link', 'tags', 'ingredients']
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
